85b3d978771f427d34988bc45ef25aea7d33a8d9
ZARIFMAC <uzbzarif@gmail.com>
2023-01-07 16:44:14 +0500

    error

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 import functions_framework
 import os
+import telegram
+from telegram import Update
+from telegram.ext import Updater,Dispatcher, CommandHandler, MessageHandler, Filters, CallbackContext
+
+# Define a function for the /start command
+def start(update: Update, context: CallbackContext) -> None:
+   # Get chat_id from update
+   chat_id = update.message.chat.id
+   # Send message to chat_id
+   bot.sendMessage(chat_id='5575549228', text='START')
+   print('Start')
+
+
+   
+# Get TOKEN from environment variable
+TOKEN = os.environ.get('TOKEN')
+# Create bot instance
+bot = telegram.Bot(token=TOKEN)
+
 @functions_framework.http
 def main(request):
-   # Get TOKEN from environment variable
-   TOKEN = os.environ.get('TOKEN')
+   # Check if request is POST
+   if request.method == 'POST':
+      bot.sendMessage(chat_id='5575549228', text='POST')
+      # Create a dispatcher instance
+      dp = Dispatcher(bot, None,workers=0)
+      # Get update from request
+      update = Update.de_json(request.get_json(force=True), bot)
+      # Add handlers
+      dp.add_handler(CommandHandler('start', start))
+      dp.process_update(update) 
+      return {'ok':TOKEN}
+
+   
    return f'TOKEN: {TOKEN}!'
