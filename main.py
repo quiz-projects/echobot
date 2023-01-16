028ed4dec16be018a0fcd8a2094d245033a6364d
ZARIFMAC <uzbzarif@gmail.com>
2023-01-16 17:47:41 +0500

    app

--- main.py
+++ main.py
@@ -2,48 +2,42 @@ import functions_framework
 import os
 import telegram
 from telegram import Update
-from telegram.ext import Updater,Dispatcher, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Application, Updater,Dispatcher, CommandHandler, MessageHandler, Filters,  ContextTypes
 # Import logging module
 import logging
 import sys
 # Define a function for the /start command
-def start(update: Update, context: CallbackContext) -> None:
+async def start(update: Update, context: ContextTypes) -> None:
    # Get chat_id from update
    chat_id = update.message.chat.id
    # Welcome message
    message = 'Welcome to my bot!'
    # Send message to chat_id
-   bot.sendMessage(chat_id=chat_id, text=message)
+   await update.message.reply_text(message)
    
-def echo(update: Update, context: CallbackContext) -> None:
-   # Get chat_id from update
-   chat_id = update.message.chat.id
-   # Get text from update
-   text = update.message.text
-   # Send message to chat_id
-   bot.sendMessage(chat_id=chat_id, text=text)
+
 
    
 # Get TOKEN from environment variable
 TOKEN = os.environ.get('TOKEN')
-# Create bot instance
-bot = telegram.Bot(token=TOKEN)
 
+# Create the Application and pass it your bot's token.
+app = Application.builder().token(TOKEN).build()
 @functions_framework.http
 def main(request):
    # Check if request is POST
    if request.method == 'POST':
       # Create a dispatcher instance
-      dp = Dispatcher(bot, None,workers=0)
+      
       # Get update from request
-      update = Update.de_json(request.get_json(force=True), bot)
+      update = Update.de_json(request.get_json(force=True), app.bot)
       # send message to log
       logging.info(f'Update: hello')
       # Add handlers
-      dp.add_handler(CommandHandler('start', start))
+      app.add_handler(CommandHandler('start', start))
       # Add handlers for echo
-      dp.add_handler(MessageHandler(Filters.text, echo))
-      dp.process_update(update) 
+      
+      app.process_update(update) 
       return {'ok':TOKEN}
 
    
