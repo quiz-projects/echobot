3acad039ce4a7746f92d96341f5dffa98bb8cb54
ZARIFMAC <uzbzarif@gmail.com>
2023-01-16 16:59:12 +0500

    echo bot 13

--- main.py
+++ main.py
@@ -3,15 +3,25 @@ import os
 import telegram
 from telegram import Update
 from telegram.ext import Updater,Dispatcher, CommandHandler, MessageHandler, Filters, CallbackContext
-
+# Import logging module
+import logging
+import sys
 # Define a function for the /start command
 def start(update: Update, context: CallbackContext) -> None:
    # Get chat_id from update
    chat_id = update.message.chat.id
+   # Welcome message
+   message = 'Welcome to my bot!'
    # Send message to chat_id
-   bot.sendMessage(chat_id='5575549228', text='START')
-   print('Start')
-
+   bot.sendMessage(chat_id=chat_id, text=message)
+   
+def echo(update: Update, context: CallbackContext) -> None:
+   # Get chat_id from update
+   chat_id = update.message.chat.id
+   # Get text from update
+   text = update.message.text
+   # Send message to chat_id
+   bot.sendMessage(chat_id=chat_id, text=text)
 
    
 # Get TOKEN from environment variable
@@ -23,13 +33,16 @@ bot = telegram.Bot(token=TOKEN)
 def main(request):
    # Check if request is POST
    if request.method == 'POST':
-      bot.sendMessage(chat_id='5575549228', text='POST')
       # Create a dispatcher instance
       dp = Dispatcher(bot, None,workers=0)
       # Get update from request
       update = Update.de_json(request.get_json(force=True), bot)
+      # send message to log
+      logging.info(f'Update: hello')
       # Add handlers
       dp.add_handler(CommandHandler('start', start))
+      # Add handlers for echo
+      dp.add_handler(MessageHandler(Filters.text, echo))
       dp.process_update(update) 
       return {'ok':TOKEN}
 
